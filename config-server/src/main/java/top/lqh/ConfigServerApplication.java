bdc941d686f5461f81943a901e33408610d1df93
lqh <15964826681@163.com>
2021-07-14 15:38:17 +0800

    配置服务器

--- config-server/src/main/java/top/lqh/ConfigServerApplication.java
+++ config-server/src/main/java/top/lqh/ConfigServerApplication.java
@@ -7,12 +7,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @date: 2021-07-14 11:07
+ * @date: 2021-07-14 15:35
  */
+@EnableConfigServer
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
